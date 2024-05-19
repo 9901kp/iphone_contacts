b50875f9e8ae758267a4c6bcda40d4bea5ab4307
9901kp <kaiypovgg@gmail.com>
2024-05-19 14:38:49 +0400

    Initial Commit

---
+++ iContacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iContacts
+//
+//  Created by Мухаммед Каипов on 19/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iContactsUITests/iContactsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  iContactsUITestsLaunchTests.swift
+//  iContactsUITests
+//
+//  Created by Мухаммед Каипов on 19/5/24.
+//
+
+import XCTest
+
+final class iContactsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
